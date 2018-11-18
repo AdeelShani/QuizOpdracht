60b33eb0c03df14365dbd273e0f81c27bea2131c
adeelhaq <adeelhaq98@gmail.com>
2018-11-18 23:45:31 +0100

    Vragen kunnen beantwoord worden.

--- QuizOpdracht/Program.cs
+++ QuizOpdracht/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace QuizOpdracht
 {
@@ -6,7 +8,40 @@ namespace QuizOpdracht
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ben je klaar om mee te doen met het Quiz? \n");
+
+            List<IVraag> vragen = new List<IVraag>{
+                new Vraag{Tekst = "Hoeveel is 20 + 1", Antwoord = "21", Categorie = "Rekenen", MoeilijkheidsGraat = 1},
+                new Vraag{Tekst = "Hoeveel is 20 - 1", Antwoord = "20", Categorie = "Rekenen", MoeilijkheidsGraat = 1},
+                new Vraag{Tekst = "Hoeveel is 1753 - 544", Antwoord = "1209", Categorie = "Rekenen", MoeilijkheidsGraat = 2},
+                new Vraag{Tekst = "Hoeveel is 1753 * 544", Antwoord = "953630", Categorie = "Rekenen", MoeilijkheidsGraat = 3},
+                new Vraag{Tekst = "Hoeveel is 100 / 5", Antwoord = "20", Categorie = "Rekenen", MoeilijkheidsGraat = 2},
+            };
+
+            // Only math questions
+            var DeVraag = from x in vragen
+                          where x.Categorie == "Rekenen"
+                          select x;
+
+            foreach (var x in DeVraag)
+            {
+                AntwoordControle(x);
+            }
+        }
+
+        static void AntwoordControle(IVraag vraag)
+        {
+            vraag.Tonen();
+            string answer = Console.ReadLine();
+
+            if (vraag.AntwoordControleren(answer))
+            {
+                Console.WriteLine("Je antwoord is juist! \n");
+            }
+            else
+            {
+                Console.WriteLine("Je antwoord is fout probeer het nogmaals! \n");
+            }
         }
     }
 }
---
+++ QuizOpdracht/Vraag.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizOpdracht
+{
+    public interface IVraag
+    {
+        string Tekst { get; set; }
+        string Antwoord { get; set; }
+        string Categorie { get; set; }
+        int MoeilijkheidsGraat { get; set; }
+
+        void Tonen();
+        bool AntwoordControleren(string response);
+    }
+
+    public class Vraag : IVraag
+    {
+
+        public string Tekst { get; set; }
+        public string Antwoord { get; set; }
+        public string Categorie { get; set; }
+        public int MoeilijkheidsGraat { get; set; }
+
+        public void Tonen()
+        {
+            Console.WriteLine(Tekst);
+        }
+
+        public bool AntwoordControleren(string response)
+        {
+            return ((Antwoord.ToLower() == response.ToLower()));
+
+        }
+    }
+}
